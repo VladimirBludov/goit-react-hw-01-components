3664df34b89b85b980123a242716101a15d5ba8a
VladimirBludov <vladimir.bloodov@gmail.com>
2022-01-10 19:56:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from "./logo.svg"
+import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
@@ -18,4 +18,4 @@ function App() {
 	)
 }
 
-export default App
+// export default App
